a66c00d8db757a2a28ece125705e901c05ab36d7
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-06-05 09:52:59 -0300

    feat: Load the Spring Initializr maven project.

---
+++ src/main/java/io/github/josiasbarreto_dev/Intervaltimer/IntervaltimerApplication.java
@@ -0,0 +1,13 @@
+package io.github.josiasbarreto_dev.Intervaltimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervaltimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntervaltimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/josiasbarreto_dev/Intervaltimer/IntervaltimerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.josiasbarreto_dev.Intervaltimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntervaltimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
